88fe763eef624d5a7636936d7657f358628fd704
Puneet Purohit <puneet_prht@yahoo.co.in>
2018-10-01 01:46:17 +0530

    Add Websocket

--- FORTUNA/FORTUNA/FORTUNA.py
+++ FORTUNA/FORTUNA/FORTUNA.py
@@ -1,1 +1,8 @@
-print('Hello World')
+print("Scrapping Mode ON")
+
+import websocket
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp('wss://api.bitfinex.com/ws/2')
+    ws.on_open = lambda self: self.send('{ "event": "subscribe", "channel": "trades", "symbol": "tBTCUSD"}')
+    ws.on_message = lambda self, evt:  print (evt)
